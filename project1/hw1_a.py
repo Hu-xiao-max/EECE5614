9a54f6fc0fd233074d6656433f33609cd9b7a87e
jerrynuthu <jerrynuthu@jerrynuthudeMacBook-Pro.local>
2025-02-03 12:39:36 -0500

    2/3

--- project1/hw1_a.py
+++ project1/hw1_a.py
@@ -1,79 +1,121 @@
 import random
 import matplotlib.pyplot as plt
-
+import numpy as np
+import scipy.stats as stats
 
 class accr():
     def __init__(self):
         self.init_q = 0
-        self.q1 = 6
-        self.q2 = 7
         self.epixilong = 0
         self.greedy = None
 
-    def acc_cacl(self, alpha, policy_greeedy):
-        self.epixilong = policy_greeedy
-        list_ACC = []
-        Q1 = self.init_q
-        Q2 = self.init_q
-        Q1_before = 0
-        Q2_before = 0
-        ACC =0 
-        for k in range(1000):
-            k += 1
-            # policy select
-            if random.random() < 1-self.epixilong:
-                self.greedy = True
-            else:
-                self.greedy = False
+    
+    def Q1_cal(self):
+        # Define the first lever's Gaussian distribution
+        mu1, sigma1 = 6, np.sqrt(15)
+        # print('21',np.random.normal(mu1, sigma1))
+        return np.random.normal(mu1, sigma1)
 
-            if self.greedy:
-                # greedy
-                if Q1 > Q2:
-                    q = self.q1
-                else:
-                    q = self.q2
-            else:
-                # Random
-                if random.random() < 0.5:
-                    q = self.q1
-                else:
-                    q = self.q2
+    def Q2_cal(self):
+        # Define the second lever's Mixture of Gaussians
+        mu21, sigma21 = 11, np.sqrt(16)
+        mu22, sigma22 = 3, np.sqrt(8)
+        if np.random.rand() < 0.5:
+            # print('21',np.random.normal(mu21, sigma21))
+            return np.random.normal(mu21, sigma21)
+        else:
+            # print('22',np.random.normal(mu22, sigma22))
+            return np.random.normal(mu22, sigma22)
+
+
+    def acc_cacl(self, policy_greeedy):
+        self.epixilong = policy_greeedy
+        Q1_sum = 0
+        Q2_sum = 0
+        for _ in range(100):
+            list_ACC = []
+            Q1 = self.init_q
+            Q2 = self.init_q
+            Q1_before = 0
+            Q2_before = 0
+            self.choose_q1 = None
+
+            ACC =0 
+
             
-            if q == self.q1:
-                Q1 = Q1 + alpha*(q-Q1_before)
-                Q2 = Q2 
-                Q1_before = Q1
-                r = Q1
-            else:
-                Q1 = Q1 
-                Q2 = Q2 + alpha*(q-Q2_before)
-                Q2_before = Q2
-                r = Q2
-            
-            ACC += r
-            average_ACC = ACC/k
-            print('k',k,'r',r,'avg_ACC',average_ACC)
-            list_ACC.append(average_ACC)
-        return list_ACC
+            for k in range(1000):
+                k += 1
+                # policy select
+                if random.random() < 1-self.epixilong:
+                    self.greedy = True
+                else:
+                    self.greedy = False
+
+                if self.greedy:
+                    # greedy
+                    if Q1 >= Q2:
+                        q = self.Q1_cal()
+                        self.choose_q1 = True
+                    else:
+                        q = self.Q2_cal()
+                else:
+                    # Random
+                    if random.random() < 0.5:
+                        q = self.Q1_cal()
+                        self.choose_q1 = True
+                    else:
+                        q = self.Q2_cal()
+                #！！！！！！！！！！！！define alpha！！！！！！！！！！！！！！！！！
+                # alpha = 1
+                # alpha = 0.9 ** k
+                # alpha = 1 / (1 + np.log(1 + k))
+                alpha= 1/k
+
+                if self.choose_q1:
+                    Q1 = Q1 + alpha*(q-Q1_before)
+                    Q2 = Q2 
+                    Q1_before = Q1
+                    r = Q1
+                    self.choose_q1 = False
+                else:
+                    Q1 = Q1 
+                    Q2 = Q2 + alpha*(q-Q2_before)
+                    Q2_before = Q2
+                    r = Q2
+                
+                ACC += r
+                average_ACC = ACC/k
+                # print('k',k,'r',r,'avg_ACC',average_ACC)
+                list_ACC.append(average_ACC)
+
+            Q1_sum += Q1
+            Q2_sum += Q2
+            # print('Q1 ', Q1)
+            # print('Q1 & Q2', Q1, Q2)
+        print('SUM', Q1_sum, Q2_sum)
+        return list_ACC, Q1_sum/100, Q2_sum/100
     
 
 if __name__ == '__main__':
     test = accr()
-
-    list1 = test.acc_cacl(alpha=1, policy_greeedy  = 0.0)
-    list2 = test.acc_cacl(alpha=1, policy_greeedy  = 0.1)
-    list3 = test.acc_cacl(alpha=1, policy_greeedy  = 0.2)
-    list4 = test.acc_cacl(alpha=1, policy_greeedy  = 0.5)
+    list1, Q1_average_100, Q2_average_100 = test.acc_cacl(policy_greeedy  = 0.0)
+    print('Epsilon  = 0.0',Q1_average_100, Q2_average_100)
+    list2, Q1_average_100, Q2_average_100 = test.acc_cacl(policy_greeedy  = 0.1)
+    print('Epsilon  = 0.1',Q1_average_100, Q2_average_100)
+    list3, Q1_average_100, Q2_average_100 = test.acc_cacl(policy_greeedy  = 0.2)
+    print('Epsilon  = 0.2',Q1_average_100, Q2_average_100)
+    list4, Q1_average_100, Q2_average_100 = test.acc_cacl(policy_greeedy  = 0.5)
+    print('Epsilon  = 0.5',Q1_average_100, Q2_average_100)
 
     plt.figure(figsize=(10, 6))
     
     # 依次绘制4条曲线，每条曲线用一个 label 标识，以便在图例中显示
-    plt.plot(list1, marker='o', markersize=3, linewidth=1, label='alpha=0.0')
-    plt.plot(list2, marker='^', markersize=3, linewidth=1, label='alpha=0.1')
-    plt.plot(list3, marker='s', markersize=3, linewidth=1, label='alpha=0.2')
-    plt.plot(list4, marker='x', markersize=3, linewidth=1, label='alpha=0.5')
+    plt.plot(list1, marker='o', markersize=3, linewidth=1, label='Epsilon=0.0')
+    plt.plot(list2, marker='^', markersize=3, linewidth=1, label='Epsilon=0.1')
+    plt.plot(list3, marker='s', markersize=3, linewidth=1, label='Epsilon=0.2')
+    plt.plot(list4, marker='x', markersize=3, linewidth=1, label='Epsilon=0.5')
 
-    plt.title('Comparison of Different Alpha Settings')
+    plt.title('Average Accumulated Reward')
     plt.xlabel('Index')
     plt.ylabel('Value')
     plt.grid(True)
